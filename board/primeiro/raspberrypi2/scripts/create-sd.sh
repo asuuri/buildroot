9726a3d7a52d570a79c60e904c6871aeb562e464
Eino Saarela Rossi <eino.saarela-rossi@stonesoft.com>
2015-10-08 09:30:03 +0300

    Allows listing disks and clearing them.

--- board/primeiro/raspberrypi2/scripts/create-sd.sh
+++ board/primeiro/raspberrypi2/scripts/create-sd.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
 if [ ! $1 ]; then
-    echo "USAGE: $0 <drive>"
+    echo "USAGE: $0 <drive>|-l"
     exit -1
 fi
 
-if [[ -b $1 ]]
-then
-    fdisk -l $1
+if [[ $1 == '-l' ]]; then
+    echo "---"
+    parted -l 2> /dev/null | grep -e '\(Model:\|Disk \/\)' | sed '/Disk /a ---' --
+    exit
+fi
+
+drive=$1
+
+if [[ -b $drive ]]; then
+    fdisk -l $drive
     read -p "Continue? (y/N): " -n 1 -r doContinue
     echo
 
-    echo $doContinue
-    if [[ "$doContinue"=="y" || "$doContinue"=="Y" ]]; then
+    if [[ $doContinue =~ ^[Yy]$ ]]; then
+        # Unmounting drives
+        echo "## Unmounting"
+        for n in "$drive*" ; do 
+            umount $n > /dev/null 2>&1
+        done
+
+        echo "## Clearing disk"
+        dd if=/dev/zero of=$drive bs=1024 count=1024
+
         echo "Continuing"
     else
         echo "Exiting..."
     fi
+else
+    echo "Could not find disk $drive"
 fi
 
